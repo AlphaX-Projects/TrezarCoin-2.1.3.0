322b898aa390ebbe2505bec07d763a85e87197a2
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2021-02-20 22:27:30 +0100

    Update misc.cpp

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -104,10 +104,7 @@ UniValue getinfo(const UniValue& params, bool fHelp)
     obj.push_back(Pair("timeoffset",    GetTimeOffset()));
     obj.push_back(Pair("connections",   (int)vNodes.size()));
     obj.push_back(Pair("proxy",         (proxy.IsValid() ? proxy.proxy.ToStringIPPort() : string())));
-    UniValue diff(UniValue::VOBJ);
-    diff.push_back(Pair("proof-of-work",        GetDifficulty()));
-    diff.push_back(Pair("proof-of-stake",       GetDifficulty(GetLastBlockIndex(chainActive.Tip(), true))));
-    obj.push_back(Pair("difficulty",    diff));
+    obj.push_back(Pair("difficulty",    (double)GetDifficulty()));
     obj.push_back(Pair("testnet",       Params().TestnetToBeDeprecatedFieldRPC()));
 #ifdef ENABLE_WALLET
     if (pwalletMain) {
